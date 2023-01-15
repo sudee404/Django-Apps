3a7371342abe70e7c131acb61831a99d5ac741f6
sudee404 <sudaise18@gmail.com>
2023-01-15 10:07:48 +0300

    user_auth installation and configuration

---
+++ user_auth/models.py
@@ -0,0 +1,57 @@
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
+from django.db import models
+
+
+class MyUserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a User with the given email, and password.
+        """
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a superuser with the given email, and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
+
+
+class MyUser(AbstractBaseUser):
+    # This is a custom user model
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    dob = models.DateField(verbose_name="date of birth")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'dob']
+
+    objects = MyUserManager()
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def save(self, *args, **kwargs):
+        self.email = self.email.lower()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        managed = True
+        verbose_name = 'User Profile'
+        verbose_name_plural = 'User Profiles'
